4e03ef05c197861bb37cce8aa4cd18b0d3e6013a
sakhawat777 <biplobbms@gmail.com>
2022-03-07 10:43:15 +0600

    Input Event Props Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Button from './components/Button';
 import Greet from './components/Greet';
 import Heading from './components/Heading';
+import Input from './components/Input';
 import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
@@ -42,6 +43,7 @@ function App() {
      <Button handleClick={(event, id) => {
        console.log("Button Clicked!", event, id);
      }}/>
+     <Input value='' handleChange={(event) => console.log(event)}/>
     </div>
   );
 }
